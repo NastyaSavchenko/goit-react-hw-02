9cdf25cda0d956e4899b1ef3ecfb5468513a7790
Анастасія Савченко <savyle4ka13@gmail.com>
2024-11-13 13:34:15 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Description from "./components/Description/Description";
+import Feedback from "./components/Feedback/Feedback";
+import Options from "./components/Options/Options";
+
+function App() {
+  return (
+    <>
+      <Description />
+      <Options />
+      <Feedback />
+    </>
+  );
+}
+
+export default App;
